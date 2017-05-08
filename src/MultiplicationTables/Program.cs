d2036e26343a71a399e963736c1d5337e9d61e4e
Kathryn Holden <Kathryn Holden>
2017-05-07 21:27:11 -0500

    game menu added

--- src/MultiplicationTables/Program.cs
+++ src/MultiplicationTables/Program.cs
@@ -11,9 +11,10 @@ namespace MultiplicationTables
         {
             // Create a dictionary of game menu name and choice number
             Dictionary<string, string> gameChoices = new Dictionary<string, string>();
-            gameChoices.Add("Multiplication Quiz", "1");
-            gameChoices.Add("Division Quiz", "2");
-            gameChoices.Add("")
+            gameChoices.Add("Multiplication Quiz", "Multiplication Quiz");
+            gameChoices.Add("Division Quiz", "Division Quiz");
+            gameChoices.Add("Addition Quiz", "Addition Quiz");
+            gameChoices.Add("Subtraction Quiz", "Subtraction Quiz");
 
             string input;
             int intA = 1;
@@ -30,45 +31,112 @@ namespace MultiplicationTables
             Random r1 = new Random(1);
             Random r2 = new Random(2);
 
-            Console.WriteLine("Welcome to Multiplication Tables!");
+            Console.WriteLine("Welcome to your Math Quiz");
 
-            do
+            while (true)
             {
-                Console.Write("How many tables do you want to run (between {0} and {1}?  ", tableMin, tableMax);
-                input = Console.ReadLine();
-                numTables = Int32.Parse(input);
-                inputTest = input == "" || numTables < tableMin || numTables > tableMax;
-            } while (inputTest); //TODO: add a comparison to tell if input is a char or a string
+                string gameChoice = GetUserSelection("View Games", gameChoices);
 
-            Console.Write("What is the largest integer (between 0 and 20) that you want to multiply by?  ");
-            input = Console.ReadLine();
-            maxInt = Int32.Parse(input);
+                Console.WriteLine("\n******* Welcome to {0}! *******", gameChoice);
 
-            //create display for tables.
-            for (int i = 0; i < numTables; i++)
-            {
-                //TODO: create random intA between zero and maxInt
-                intA = r1.Next(maxInt);
-                //TODO: create ramdom intB between zero and maxInt
-                intB = r2.Next(maxInt);
-                Console.WriteLine("What is {0} times {1}? ", intA, intB);
-                input = Console.ReadLine();
-                answer = Int32.Parse(input);
-                if (answer == intA * intB)
+
+                if (gameChoice.Equals("Multiplication Quiz"))
                 {
-                    Console.WriteLine("Correct!");
-                    numCorrect++;
+                    do
+                    {
+                        Console.Write("How many tables do you want to run (between {0} and {1}?  ", tableMin, tableMax);
+                        input = Console.ReadLine();
+                        numTables = Int32.Parse(input);
+                        inputTest = input == "" || numTables < tableMin || numTables > tableMax;
+                    } while (inputTest); //TODO: add a comparison to tell if input is a char or a string
+
+                    Console.Write("What is the largest integer (between 0 and 20) that you want to multiply by?  ");
+                    input = Console.ReadLine();
+                    maxInt = Int32.Parse(input);
+
+                    //create display for tables.
+                    for (int i = 0; i < numTables; i++)
+                    {
+                        //TODO: create random intA between zero and maxInt
+                        intA = r1.Next(maxInt);
+                        //TODO: create ramdom intB between zero and maxInt
+                        intB = r2.Next(maxInt);
+                        Console.WriteLine("\nWhat is {0} times {1}? ", intA, intB);
+                        input = Console.ReadLine();
+                        answer = Int32.Parse(input);
+                        if (answer == intA * intB)
+                        {
+                            Console.WriteLine("Correct!");
+                            numCorrect++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("That wasn't correct.");
+                        }
+
+                    }
+                    //at the end display the number correct out of the total number of tables
+                    Console.WriteLine("\nYou got {0} out of {1} correct.", numCorrect, numTables);
+
+                    Console.ReadLine();
+                }
+                else if (gameChoice.Equals("Division Quiz"))
+                {
+                    Console.WriteLine("Quiz Under Construction...check back.");
+
+                    Console.ReadLine();
                 }
                 else
                 {
-                    Console.WriteLine("That wasn't correct.");
+                    Console.WriteLine("Quiz Under Construction...check back.");
+
+                    Console.ReadLine();
+
+                }
+            }
+        }
+
+
+
+
+
+        private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices)
+        {
+            int choiceIdx;
+            bool isValidChoice = false;
+            string[] choiceKeys = new string[choices.Count];
+
+            int i = 0;
+
+            foreach (KeyValuePair<string, string> choice in choices)
+            {
+                choiceKeys[i] = choice.Key;
+                i++;
+            }
+
+            do
+            {
+                Console.WriteLine("\n" + choiceHeader + " by:");
+
+                for (int j = 0; j < choiceKeys.Length; j++)
+                {
+                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);
                 }
 
-            }
-            //at the end display the number correct out of the total number of tables
-            Console.WriteLine("\nYou got {0} out of {1} correct.", numCorrect, numTables);
+                string input = Console.ReadLine();
+                choiceIdx = int.Parse(input);
 
-            Console.ReadLine();
+                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)
+                {
+                    Console.WriteLine("Invalid choice.  Try again.");
+                }
+                else
+                {
+                    isValidChoice = true;
+                }
+            } while (!isValidChoice);
+
+            return choiceKeys[choiceIdx];
         }
     }
 }
